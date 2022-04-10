d0c04d61ad84386186637d1f72b576ccd0590ab2
EslamAref75 <https://github.com/EslamAref75>
2022-04-10 17:25:57 +0200

    convert class to hook

--- src/App.js
+++ src/App.js
@@ -1,70 +1,46 @@
-import React from "react";
-import * as BooksAPI from "./BooksAPI";
+import { useState, useEffect } from "react";
 import "./App.css";
+import * as BooksAPI from "./BooksAPI";
 import BookList from "./BookList";
 import { Routes, Route } from "react-router-dom";
 import BookSearch from "./BookSearch";
 import NotFound from "./NotFound";
 
-class BooksApp extends React.Component {
-  state = {
-    books: [],
-  };
+function BooksApp() {
+  const [books, setBooks] = useState([]);
 
-  componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState(() => ({ books }));
-    });
-  }
+  useEffect(() => {
+    getBooks();
+  }, [books]);
+
+  const getBooks = async () => {
+    const receivedBooks = await BooksAPI.getAll();
+    setBooks(receivedBooks);
+  };
 
   // Handle book shelves selection in the book list
-  handleChange = (book, shelf) => {
+  const handleChange = async (book, shelf) => {
     book.shelf = shelf;
-    BooksAPI.update(book, shelf).then(
-      this.setState((currentState) => {
-        return {
-          books: currentState.books.map((b) => (b.id === book.id ? book : b)),
-        };
-      })
-    );
+    await BooksAPI.update(book, shelf);
+    getBooks();
   };
 
-  // Reload the page to get the updated books from search path
-  reloadPage = () => {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books });
-    });
-  };
-
-  render() {
-    return (
-      <div className="app">
-        <Routes>
-          <Route path="*" element={<NotFound />} />
-          <Route
-            exact
-            path="/"
-            element={
-              <BookList
-                books={this.state.books}
-                handleChange={this.handleChange}
-              />
-            }
-          />
-          <Route
-            path="/search"
-            element={
-              <BookSearch
-                books={this.state.books}
-                handleChange={this.handleChange}
-                reloadPage={this.reloadPage}
-              />
-            }
-          />
-        </Routes>
-      </div>
-    );
-  }
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route
+          exact
+          path="/"
+          element={<BookList books={books} handleChange={handleChange} />}
+        />
+        <Route
+          path="/search"
+          element={<BookSearch books={books} handleChange={handleChange} />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default BooksApp;
--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,23 +1,22 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import * as BooksAPI from "./BooksAPI";
 import { Link } from "react-router-dom";
 import Books from "./Books";
 import { DebounceInput } from "react-debounce-input";
 
-class BookSearch extends Component {
-  state = {
-    query: "",
-    books: [],
-  };
+function BookSearch(props) {
+  const [query, setQuery] = useState("");
+  const [books, setBooks] = useState([]);
+  const { handleChange } = props;
 
   // Get books from api search path according to the query
-  getBooks = async (query) => {
+  const getBooks = async (query) => {
     try {
       const books = await BooksAPI.search(query);
       if (books.error) {
         return console.warn("Please provide a query in the search input field");
       } else if (books) {
-        const newBooks = this.shelfHandler(books);
+        const newBooks = shelfHandler(books);
         return newBooks;
       }
     } catch (error) {
@@ -26,8 +25,8 @@ class BookSearch extends Component {
   };
 
   // Update the shelves in books from search api results by matching book id
-  shelfHandler(newBooks) {
-    const { books } = this.props;
+  const shelfHandler = (newBooks) => {
+    const { books } = props;
     const booksUpdate = newBooks.map((newBook) => {
       books.forEach((oldBook) => {
         if (oldBook.id === newBook.id) {
@@ -37,85 +36,77 @@ class BookSearch extends Component {
       return newBook;
     });
     return booksUpdate;
-  }
+  };
 
   // Update the compenent to get the matched books in the query input
-  updateQuery = async (e) => {
+  const updateQuery = async (e) => {
     const query = e.target.value;
-    this.setState({ query });
+    setQuery(query);
     if (query === "") {
-      this.setState({ books: [] });
+      setBooks({ books: [] });
     } else if (query) {
-      const books = await this.getBooks(query);
+      const books = await getBooks(query);
       if (books) {
-        this.setState({ books });
+        setBooks(books);
       } else {
-        this.setState({ books: [] });
+        setBooks({ books: [] });
       }
     }
   };
 
-  render() {
-    const { books } = this.state;
-    const { handleChange, reloadPage } = this.props;
-
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link to="/">
-            <button
-              className="close-search"
-              onClick={() => {
-                reloadPage();
-              }}
-            >
-              Close
-            </button>
-          </Link>
-          <div className="search-books-input-wrapper">
-            {/* Use DebounceInput to handle the minimal length of text 
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <Link to="/">
+          <button
+            className="close-search"
+           
+          >
+            Close
+          </button>
+        </Link>
+        <div className="search-books-input-wrapper">
+          {/* Use DebounceInput to handle the minimal length of text 
             and the notification debounce timeout in ms */}
-            <DebounceInput
-              minLength={3}
-              debounceTimeout={500}
-              onChange={this.updateQuery}
-              type="text"
-              placeholder="Search by title or author"
-            />
-          </div>
-        </div>
-        <div className="search-books-results">
-          {books.length > 0 && (
-            <ol className="books-grid">
-              {books.map((book) => (
-                <Books key={book.id} handleChange={handleChange} book={book} />
-              ))}
-            </ol>
-          )}
-          {books.length <= 0 && (
-            <div>
-              <h3>Search Terms:</h3>
-              <p>
-                Android, Art, Artificial Intelligence, Astronomy, Austen,
-                Baseball, Basketball, Bhagat, Biography, Brief, Business, Camus,
-                Cervantes, Christie, Classics, Comics, Cook, Cricket, Cycling,
-                Desai, Design, Development, Digital Marketing, Drama, Drawing,
-                Dumas, Education, Everything, Fantasy, Film, Finance, First,
-                Fitness, Football, Future, Games, Gandhi, Homer, Horror, Hugo,
-                Ibsen, Journey, Kafka, King, Lahiri, Larsson, Learn, Literary
-                Fiction, Make, Manage, Marquez, Money, Mystery, Negotiate,
-                Painting, Philosophy, Photography, Poetry, Production,
-                Programming, React, Redux, River, Robotics, Rowling, Satire,
-                Science Fiction, Shakespeare, Singh, Swimming, Tale, Thrun,
-                Time, Tolstoy, Travel, Ultimate, Virtual Reality, Web
-                Development, iOS
-              </p>
-            </div>
-          )}
+          <DebounceInput
+            minLength={3}
+            debounceTimeout={500}
+            onChange={updateQuery}
+            type="text"
+            value={query}
+            placeholder="Search by title or author"
+          />
         </div>
       </div>
-    );
-  }
+      <div className="search-books-results">
+        {books.length > 0 ? (
+          <ol className="books-grid">
+            {books.map((book) => (
+              <Books key={book.id} handleChange={handleChange} book={book} />
+            ))}
+          </ol>
+        ) : (
+          <div>
+            <h3>Search Terms:</h3>
+            <p>
+              Android, Art, Artificial Intelligence, Astronomy, Austen,
+              Baseball, Basketball, Bhagat, Biography, Brief, Business, Camus,
+              Cervantes, Christie, Classics, Comics, Cook, Cricket, Cycling,
+              Desai, Design, Development, Digital Marketing, Drama, Drawing,
+              Dumas, Education, Everything, Fantasy, Film, Finance, First,
+              Fitness, Football, Future, Games, Gandhi, Homer, Horror, Hugo,
+              Ibsen, Journey, Kafka, King, Lahiri, Larsson, Learn, Literary
+              Fiction, Make, Manage, Marquez, Money, Mystery, Negotiate,
+              Painting, Philosophy, Photography, Poetry, Production,
+              Programming, React, Redux, River, Robotics, Rowling, Satire,
+              Science Fiction, Shakespeare, Singh, Swimming, Tale, Thrun, Time,
+              Tolstoy, Travel, Ultimate, Virtual Reality, Web Development, iOS
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default BookSearch;
